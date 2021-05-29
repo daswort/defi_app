42278f2cbd6c9b2e0e7fa4b935a357fd98eb5194
Jacob Vidal <firstcommand@gmail.com>
2021-05-29 17:42:14 -0400

    3. Adding working token to the farm

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
-const TokenFarm = artifacts.require("TokenFarm");
+const PndrgnToken = artifacts.require('PndrgnToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
-};
+module.exports = async function(deployer, netwotk, accounts) {
+
+  // Deploy Fake Dai Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()  
+
+  // Deploy Pndrgn Token
+  await deployer.deploy(PndrgnToken)
+  const pndrgnToken = await PndrgnToken.deployed()
+
+  // Deploy Token Farm
+  await deployer.deploy(TokenFarm, pndrgnToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  // Transfer all tokens to TokenFarm (1 million)
+  await pndrgnToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // Transfer 100 Fake DAI token to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
+
+}
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.5.0;
 
+import "./PndrgnToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
 
 	string public name = "Pndrgn Token Farm";
+	PndrgnToken public pndrgnToken;
+	DaiToken public daiToken;
+
+	constructor(PndrgnToken _pndrgnToken, DaiToken _daiToken) public {
+		pndrgnToken = _pndrgnToken;
+		daiToken = _daiToken;
+	}
 }
